e40b86de6043b61216ab3278a99e876d29d1010e
k1R4 <srijiith@outlook.com>
2024-10-01 21:16:10 +0530

    [Feature] Combine usernames

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import csv
+from usermap import users
 
 README = """# team bi0s
 ## CTF Solves
@@ -17,6 +18,7 @@ Use `run.py` to see your solve stats.
 filenames = os.listdir('solves/')
 filenames = ['solves/'+i for i in filenames]
     
+
 row_set = set()    
 for file in filenames:
     for line in open(file).readlines():
@@ -25,7 +27,8 @@ with open('allsolves.csv','w') as f:
     for row in row_set:
         if row.endswith('000'):
             f.write(row+'\n')
-            
+
+
 counts = dict()
 solves_set = set()
 for file in filenames:
@@ -35,14 +38,22 @@ for file in filenames:
         if row[-1].endswith('000'):
             solves_set.add(','.join(row))
             for username in [i.strip() for i in row[2].split(',')]:
+                for k,v in users.items():
+                    if username in v:
+                        username = k
+                        break
                 if username not in counts:
                     counts[username] = 0
                 counts[username] += 1
 
+
 solves = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+
 
 while True:
     ch = input("1. User solves\n2. All solves\n3. Exit\nEnter choice: ")
+
+
     if ch == '1':
         inp = input("Save to file? (y/n): ")
         if inp.lower() == 'y':
@@ -50,21 +61,34 @@ while True:
         csv_reader = csv.reader(open('allsolves.csv').readlines(), delimiter=',')
         name = input("Enter name: ")
         count = 0
+
+        if name in users.keys():
+            usernames = users[name]
+        else:
+            usernames = [name]
+
         for row in csv_reader:
-            if name.lower() in [r.lower() for r in row[2].split(', ')]:
+            if len(set([r for r in row[2].split(', ')]).intersection(usernames)):
                 if inp.lower() == 'y':
                     csv_writer.writerow(row)
                 row_ = [i for i in row]
-                row_[2] = row_[2].replace(name, f"\033[1;31;40m{name}\033[0m")
+
+                for username in usernames:
+                    row_[2] = row_[2].replace(username, f"\033[1;31;40m{username}\033[0m")
+
                 print(' | '.join(row_))
                 count+=1
         print("Total solves:",count)
+
+
     elif ch == '2':
         readme = open('README.md', 'w')
         readme.write(README)
         for i,j in solves.items():
             readme.write(f"| {i} | {j} |\n")
         print("README.md updated")
+
+
     elif ch == '3':
         break
     else:
---
+++ usermap.py
@@ -0,0 +1,22 @@
+users = {
+    # Pwn
+    "justk1r4": ["justk1r4", "k1R4"],
+    "spektre": ["spektre", "spektr.e"],
+    "tourpran": ["tourpran"],
+    "r0r1": ["_r0r1_", "rori"],
+    "b4tmite": ["theb4tmite", "b4tmite"],
+    "heartstoller": ["heartstoller"],
+    "SkyLance": ["silence_"],
+
+    # Web
+    # TODO
+
+    # Crypto
+    # TODO
+
+    # Rev
+    # TODO
+
+    # Forensics
+    # TODO
+}
